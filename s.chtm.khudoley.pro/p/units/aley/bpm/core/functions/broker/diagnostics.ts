530faceedc3a491d8cb8b3dfd50d769453bf5af9
Andrey Khudoley <andrey@khudoley.pro>
2026-06-25 01:55:54 +0300

    chore(bpm): удалил workspace aley bpm
    - удалён каталог p/units/aley/bpm целиком
    
    - удалены проекты core, interfaces/max и interfaces/web с документацией, API, UI и таблицами

--- s.chtm.khudoley.pro/p/units/aley/bpm/core/functions/broker/diagnostics.ts
+++
@@ -1,8 +0,0 @@
-import { getBrokerDiagnostics } from '../../lib/broker/internalApi.lib'
-
-export const brokerDiagnosticsFunction = app.function(
-  '/broker/diagnostics',
-  async (ctx, params: Parameters<typeof getBrokerDiagnostics>[1] = {}) => {
-    return getBrokerDiagnostics(ctx, params)
-  }
-)
--- s.chtm.khudoley.pro/p/units/aley/bpm/interfaces/max/api/max/poll/reset-marker.ts
+++
@@ -1,9 +0,0 @@
-// @shared-route
-import { requireAccountRole } from '@app/auth'
-import * as settingsLib from '../../../lib/settings.lib'
-
-export const maxPollResetMarkerRoute = app.post('/', async (ctx, _req) => {
-  requireAccountRole(ctx, 'Admin')
-  await settingsLib.setSetting(ctx, settingsLib.SETTING_KEYS.MAX_POLLING_MARKER, null)
-  return { success: true, marker: null }
-})
